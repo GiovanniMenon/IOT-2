4741bdffaf4a342c27c617204414f61202b484a7
Menny <giovanni.menon15@gmail.com>
2025-02-12 17:43:25 +0100

    Final Working Example

--- InterceptionB6/B6_field.py
+++ InterceptionB6/B6_field.py
@@ -8,42 +8,37 @@ import logging
 FORMAT = ('%(asctime)-15s %(threadName)-15s'
           ' %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
 logging.basicConfig(format=FORMAT)
+logging.disable(logging.CRITICAL)
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-LOCAL_ADDRESS = 'localhost'  
-PORT = 5030  
+SERVERS = [
+    ("localhost", 5030, {
+        1: ModbusSlaveContext(hr=ModbusSequentialDataBlock(0, [10] * 100)),
+        5: ModbusSlaveContext(hr=ModbusSequentialDataBlock(0, [30] * 100)),
+    }),
+    ("localhost", 5020, {
+        56: ModbusSlaveContext(hr=ModbusSequentialDataBlock(0, [50] * 100)),
+        77: ModbusSlaveContext(hr=ModbusSequentialDataBlock(0, [70] * 100)),
+    }),
+    ("localhost", 5010, {
+        17: ModbusSlaveContext(hr=ModbusSequentialDataBlock(0, [10] * 100)),
+        59: ModbusSlaveContext(hr=ModbusSequentialDataBlock(0, [30] * 100)),
+    }),
+    ("localhost", 5000, {
+        26: ModbusSlaveContext(hr=ModbusSequentialDataBlock(0, [50] * 100)),
+        197: ModbusSlaveContext(hr=ModbusSequentialDataBlock(0, [70] * 100)),
+    }),
+]
 
+async def start_server(ip, port, slaves):
+    context = ModbusServerContext(slaves=slaves, single=False)
+    print(f"Starting Modbus Server on {ip}:{port}")
+    await StartAsyncTcpServer(context, address=(ip, port))
 
-def get_context():
-    slaves = {
-        1: ModbusSlaveContext(
-            hr=ModbusSequentialDataBlock(0, [10] * 100), 
-            ir=ModbusSequentialDataBlock(0, [20] * 100), 
-        ),
-        5: ModbusSlaveContext(
-            hr=ModbusSequentialDataBlock(0, [30] * 100),
-            ir=ModbusSequentialDataBlock(0, [40] * 100),
-        ),
-        56: ModbusSlaveContext(
-            hr=ModbusSequentialDataBlock(0, [50] * 100),
-            ir=ModbusSequentialDataBlock(0, [60] * 100),
-        ),
-        77: ModbusSlaveContext(
-            hr=ModbusSequentialDataBlock(0, [70] * 100),
-            ir=ModbusSequentialDataBlock(0, [80] * 100),
-        ),
-        190: ModbusSlaveContext(
-            hr=ModbusSequentialDataBlock(0, [90] * 100),
-            ir=ModbusSequentialDataBlock(0, [10] * 100),
-        ),
-    }
-    return ModbusServerContext(slaves=slaves, single=False)
-
-
-async def run_server():
-    context = get_context()
-    await StartAsyncTcpServer(context, address=(LOCAL_ADDRESS, PORT))
+async def main():
+    tasks = [start_server(ip, port, slaves) for ip, port, slaves in SERVERS]
+    await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
-    asyncio.run(run_server())
+    asyncio.run(main())
