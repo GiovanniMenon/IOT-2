d4748e6f0c74be53b8d33745858c843b8e3fde1e
ugniuspetryla <ugniuspe@gmail.com>
2025-02-14 15:25:32 +0200

    correct package capturing

--- FabricationB2/B2_spoofer.py
+++ FabricationB2/B2_spoofer.py
@@ -8,14 +8,16 @@ logging.basicConfig(format=FORMAT)
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-def sniff_modbus_packets(interface="Loopback Pseudo-Interface 1"):
+def sniff_modbus_packets(interface="Adapter for loopback traffic capture"):
     """Sniff Modbus packets over the specified interface"""
-    capture = pyshark.LiveCapture(interface=interface, display_filter="modbus")
+    capture = pyshark.LiveCapture(interface=interface)
     log.info("Sniffing Modbus packets...")
-    for packet in capture.sniff_continuously():
+    packet_count = 0
+    for packet in capture.sniff_continuously():  # Limit to 10 packets for testing
         log.info(f"Captured packet: {packet}")
+        # Check if the packet has a TCP layer and whether it has a Raw layer
         print(packet)
 
 # To allow it to be used as a script as well:
 if __name__ == "__main__":
-    sniff_modbus_packets(interface="Loopback Pseudo-Interface 1")  # Adjust the interface name if needed
+    sniff_modbus_packets()
