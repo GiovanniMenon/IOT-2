de644ff7eeda0c45e534321e000d9191f2c5e31d
ugniuspetryla <ugniuspe@gmail.com>
2025-02-15 11:48:20 +0200

    more cleanup

--- FabricationB2/B2_replayer.py
+++ FabricationB2/B2_replayer.py
@@ -3,7 +3,12 @@ import pyshark
 
 capture_filter = "tcp.port == 5030"
 
-cap = pyshark.LiveCapture(interface='Adapter for loopback traffic capture', display_filter=capture_filter)
+# CHANGE THIS IF IT DOESN'T CORRESPOND TO THE INTERFACE FOR YOUR SYSTEM
+INTERFACE = 'Adapter for loopback traffic capture'
+LOCAL_ADDRESS = 'localhost'
+PORT = 5030
+
+cap = pyshark.LiveCapture(INTERFACE, display_filter=capture_filter)
 
 payload = None
 
@@ -18,7 +23,7 @@ print("found a packet with payload")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     try:
         sock.settimeout(60)
-        sock.connect(("localhost", 5030))  # Connect to Modbus server
+        sock.connect((LOCAL_ADDRESS, PORT))  # Connect to Modbus server
         print("Connected, replaying the message")
 
         # Convert the cleaned string (which is now a valid hex string) to bytes
